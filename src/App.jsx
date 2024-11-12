98c14e2ee78be9d8473f717319cf5f434c0bec32
sandesh <sandeshbelbase11@gmail.com>
2024-11-13 10:34:50 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Welcome to Boost up roastering system.</div>
+    </>
+  );
+}
+
+export default App;
