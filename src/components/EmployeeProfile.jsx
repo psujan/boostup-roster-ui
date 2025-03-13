159ccb1ca39cf07186310e7d54dbe0d5d902c681
suzit7 <suzit.paudel10@gmail.com>
2025-03-13 17:46:36 +1100

    changes updated

--- src/components/EmployeeProfile.jsx
+++ src/components/EmployeeProfile.jsx
@@ -12,6 +12,7 @@ import {
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import BackButton from "../commonComponents/BackButton";
 import "../index.css";
+import ECard from "./ECard";
 
 const EmployeeProfile = () => {
   return (
@@ -59,75 +60,9 @@ const EmployeeProfile = () => {
   sx={{ width: "200px" }}
   inputProps={{ style: { height: "15px", padding: "10px" } }} 
 />
-      <Card
-        sx={{
-          maxWidth: "294px",
-          height : "300px",
-          textAlign: "center",
-          p: 2,
-          borderRadius: "6px",
-          // boxShadow: 3,
-          backgroundColor: '#FCFBFB',
-          mt: '40px'
-        }}
-      >
-        <Avatar
-          src="https://via.placeholder.com/150"
-          alt="James Wilson"
-          sx={{ width: 80, height: 80, margin: "auto", mb: 2 }}
-        />
-        <CardContent>
-          <Typography sx={{fontFamily : 'Inter', fontWeight : '500'}}>
-            James Wilson
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            jameswilsom@gmail.com
-          </Typography>
-          <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
-            +61 417 456 789
-          </Typography>
-          <Stack direction="row" spacing={2} justifyContent="center">
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "var(--primaryColor)",
-                width: "105px",
-                height: "35px",
-                padding: "10px",
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  textTransform: "none",
-                }}
-              >
-                Plan Schedule
-              </Typography>
-            </Button>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "var(--secondaryColor)",
-                color: "black",
-                width: "105px",
-                height: "35px",
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: "12px",
-                  fontWeight: "500",
-                  textTransform: "none",
-                }}
-              >
-                View Profile
-              </Typography>
-            </Button>
-          </Stack>
-        </CardContent>
-      </Card>
+      <ECard/>
+     
+
       </Box>
     </>
   );
