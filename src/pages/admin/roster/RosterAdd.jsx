0d5920bb22a4773da467181687be1939e6d288ac
Sujan Poudel <suzzanpoudel1996@gmail.com>
2025-04-16 17:18:02 +1000

    feat : add roster

--- src/pages/admin/roster/RosterAdd.jsx
+++ src/pages/admin/roster/RosterAdd.jsx
@@ -1,41 +1,20 @@
-import {
-  Box,
-  Button,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Heading from "../../../components/common/Heading.jsx";
 import BaseLayout from "../../../components/common/BaseLayout.jsx";
 import ListIcon from "@mui/icons-material/NorthEast";
 import { useNavigate } from "react-router-dom";
-import Grid from "@mui/material/Grid2";
 import { useEffect, useState } from "react";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import EmployeeSelectDropdown from "../../../components/features/EmployeeSelectDropdown.jsx";
-import { useLoader } from "../../../utils/context/LoaderContext.jsx";
 import api from "../../../services/api.jsx";
+import RosterAddForm from "./partials/RosterAddForm.jsx";
+import { useLoader } from "../../../utils/context/LoaderContext.jsx";
 
 export default function RosterAdd() {
   const navigate = useNavigate();
   const { showLoader, hideLoader } = useLoader();
-  const [from, setFrom] = useState();
-  const [jobId, setJobId] = useState();
-  const [jobs, setJobs] = useState([]);
 
   const [employeeList, setEmployeeList] = useState([]);
+  const [jobs, setJobs] = useState([]);
 
-  // ui event handlers
-  const handleJobchange = (id) => {
-    console.log(id);
-    if (!id) {
-      return;
-    }
-
-    //set starttime, endtime and job hours based on job id
-  };
-  
   //api call
   // Get All Employee List without pagination for dropdown select
   const getEmployeeList = () => {
@@ -94,146 +73,7 @@ export default function RosterAdd() {
           <ListIcon sx={{ marginLeft: "10px" }} />
         </Button>
       </Box>
-      <Box className="content-box">
-        <div className="roster-date">
-          <div>
-            <h5>Please Select Roster Date</h5>
-          </div>
-        </div>
-        <div style={{ marginBottom: "30px" }}>
-          <Grid
-            container
-            columnSpacing={2}
-            columns={12}
-            sx={{
-              alignItems: "baseline",
-              justifyContent: "flex-start",
-            }}
-          >
-            <Grid size={{ sm: 12, md: 6 }}>
-              <InputLabel className="base-input-label"> From</InputLabel>
-              <DatePicker
-                className="base-input"
-                value={from}
-                sx={{ padding: "4px", height: "20px" }}
-                onChange={(v) => setFrom(v)}
-                placeholder="From"
-              />
-            </Grid>
-            <Grid size={{ sm: 12, md: 6 }}>
-              <InputLabel className="base-input-label"> To</InputLabel>
-              <DatePicker
-                className="base-input"
-                value={from}
-                sx={{ padding: "4px", height: "20px" }}
-                onChange={(v) => setFrom(v)}
-                placeholder="From"
-              />
-            </Grid>
-          </Grid>
-        </div>
-      </Box>
-      <Box className="content-box">
-        <div className="roster-date">
-          <div>
-            <h5>Your are adding roster from -- to --</h5>
-          </div>
-        </div>
-        <div style={{ marginBottom: "30px" }}>
-          <Grid
-            container
-            columnSpacing={3}
-            rowSpacing={2}
-            columns={12}
-            sx={{
-              alignItems: "baseline",
-              justifyContent: "flex-start",
-            }}
-          >
-            <Grid size={{ sm: 12, md: 12 }}>
-              <InputLabel className="base-input-label" id="roster-job-label">
-                Job
-              </InputLabel>
-              <select
-                id="roster-job-label"
-                className="base-input-select"
-                onChange={(e) => handleJobchange(e.target.value)}
-              >
-                <option value=""></option>
-                {jobs.length
-                  ? jobs.map((job) => {
-                      return (
-                        <option value={job.id} key={job.id}>
-                          {job.title}
-                        </option>
-                      );
-                    })
-                  : ""}
-              </select>
-            </Grid>
-            <Grid size={{ sm: 12, md: 4 }}>
-              <InputLabel className="base-input-label">Start Time</InputLabel>
-              <TextField
-                type="email"
-                id="login-email"
-                variant="outlined"
-                className="base-input"
-                onChange={(e) => setFrom(e.target.value)}
-              />
-            </Grid>
-            <Grid size={{ sm: 12, md: 4 }}>
-              <InputLabel className="base-input-label">End Time</InputLabel>
-              <TextField
-                type="email"
-                id="login-email"
-                variant="outlined"
-                className="base-input"
-                onChange={(e) => setFrom(e.target.value)}
-              />
-            </Grid>
-            <Grid size={{ sm: 12, md: 4 }}>
-              <InputLabel className="base-input-label">Total Hours</InputLabel>
-              <TextField
-                type="email"
-                id="login-email"
-                variant="outlined"
-                className="base-input"
-                onChange={(e) => setFrom(e.target.value)}
-              />
-            </Grid>
-            <Grid size={{ sm: 12, md: 12 }}>
-              <InputLabel className="base-input-label">Staff</InputLabel>
-              <EmployeeSelectDropdown
-                employeeList={employeeList}
-                style={{ marginTop: "13px" }}
-                customClass="base-select-dropdown"
-              />
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            columnSpacing={3}
-            rowSpacing={2}
-            columns={12}
-            sx={{
-              marginTop: "24px",
-              alignItems: "baseline",
-              justifyContent: "flex-end",
-            }}
-          >
-            <Grid size={{ sm: 8, md: 3 }}>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={() => {}}
-                sx={{ backgroundColor: "#1E7E51", height: 48 }}
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </div>
-      </Box>
+      <RosterAddForm jobs={jobs} employeeList={employeeList} />
     </BaseLayout>
   );
 }
