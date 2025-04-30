f92deca0a8f5962fe4e55a7b9ad15e4036062030
Sujan Poudel <suzzanpoudel1996@gmail.com>
2025-04-30 16:10:57 +1000

    feat : employee leave request

--- src/pages/employee/EmployeeShiftDetail/EmployeeShiftDetails.jsx
+++ src/pages/employee/EmployeeShiftDetail/EmployeeShiftDetails.jsx
@@ -2,31 +2,33 @@ import { Box, Button, Divider } from "@mui/material";
 import ShiftDetails from "./partials/ShiftDetails";
 import BackButton from "../../../components/common/BackButton";
 import { useNavigate } from "react-router-dom";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import api from "../../../services/api";
 import { useLoader } from "../../../utils/context/LoaderContext";
 import { useEffect, useState } from "react";
 export default function EmployeeShiftDetails() {
-  const {showLoader , hideLoader} = useLoader()
+  const { showLoader, hideLoader } = useLoader();
   const navigate = useNavigate();
-  const {id} = useParams();
-  const [shift, setShift] =  useState({});
-  const getShiftDetail = ()=>{
-    showLoader()
-    api.get('/api/v1/roster/'+id).then((res)=>{
-      setShift(res?.data?.data);
-    })
-    .catch((err)=>{
-      console.error(err)
-    })
-    .finally(()=>{
-      hideLoader()
-    })
-  }
+  const { id } = useParams();
+  const [shift, setShift] = useState({});
+  const getShiftDetail = () => {
+    showLoader();
+    api
+      .get("/api/v1/roster/" + id)
+      .then((res) => {
+        setShift(res?.data?.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        hideLoader();
+      });
+  };
 
-  useEffect(()=>{
-    getShiftDetail()
-  },[])
+  useEffect(() => {
+    getShiftDetail();
+  }, []);
 
   return (
     <>
@@ -54,7 +56,7 @@ export default function EmployeeShiftDetails() {
             View All
           </Button>
         </Box>
-        <ShiftDetails shift={shift}/>
+        <ShiftDetails shift={shift} />
         <Box sx={{ marginTop: "40px" }}>
           <Divider sx={{ margin: "10px 0" }} />
 
@@ -62,7 +64,9 @@ export default function EmployeeShiftDetails() {
             variant="contained"
             color="primary"
             sx={{ width: "100%" }}
-            onClick={() => navigate("/employee-leave/add?roster_id=")}
+            onClick={() =>
+              navigate("/employee-leave/add?roster_id=" + shift.id)
+            }
           >
             <span>I Can't Attend This Shift</span>
           </Button>
