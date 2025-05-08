76d6ce7556e7cb5a6f23b873756b5bbe856ab261
Sujan Poudel <suzzanpoudel1996@gmail.com>
2025-05-08 14:46:54 +1000

    fix : username

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -3,7 +3,9 @@ import { Box } from "@mui/material";
 import QuickActions from "./partials/QuickActions";
 import RosterList from "../../../components/features/roster/RosterList";
 import Overview from "./partials/Overview";
+import { isAuthenticated } from "../../../utils/auth";
 const Dashboard = () => {
+  const { user } = isAuthenticated();
   return (
     <Box>
       <Box
@@ -14,7 +16,7 @@ const Dashboard = () => {
         sx={{ borderRadius: 2 }}
       >
         <h1>
-          <span className="clr-primary">Hi</span> Boostup Admin
+          <span className="clr-primary">Hi</span> {user?.fullName}
         </h1>
         <QuickActions />
       </Box>
